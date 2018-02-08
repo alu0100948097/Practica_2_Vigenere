c90821d5bd304a5ed48a45689563e78c8e867c78
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2018-02-08 17:11:22 +0000

    Creación de método para dividir el texto original

--- lib/vigenere.rb
+++ lib/vigenere.rb
@@ -2,20 +2,25 @@ require 'colorize'
 
 class Vigenere
 	
-	attr_reader :alfabeto, :mensaje, :clave
+	attr_reader :alfabeto, :mensaje, :clave, :div_mensaje
 	
 	def initialize
 		@alfabeto=('A'..'Z').to_a
 		@mensaje=""
 		@clave=[]
+		@div_mensaje=[]
 	end
 	
 	def mensaje=(m)
-		@mensaje=m.delete(' ').split('')
+		@mensaje=m.delete(' ')
 	end
 	
 	def clave=(c)
 		@clave=c.delete(' ').split('')
+	end
+	
+	def div_mensaje
+		@div_mensaje=@mensaje.scan(/.{1,#{@clave.length}}/)
 	end
 	
 end
