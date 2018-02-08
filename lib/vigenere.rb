bed9488ba782e10850cb3550db5a4d5df6a70c6b
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2018-02-08 10:22:15 +0000

    Creación de método para descartar espacios del texto original

--- lib/vigenere.rb
+++ lib/vigenere.rb
@@ -2,10 +2,15 @@ require 'colorize'
 
 class Vigenere
 	
-	attr_reader :alfabeto
+	attr_reader :alfabeto, :mensaje
 	
 	def initialize
 		@alfabeto=('A'..'Z').to_a
+		@mensaje=""
+	end
+	
+	def mensaje=(m)
+		@mensaje=m.delete(' ')
 	end
 	
 end
