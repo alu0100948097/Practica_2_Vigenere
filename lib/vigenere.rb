b873ccb8c1725050c4a118db1a75f51aa744101f
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2018-02-11 12:22:30 +0000

    Array de dos dimensiones con cada letra del mensaje

--- lib/vigenere.rb
+++ lib/vigenere.rb
@@ -21,6 +21,11 @@ class Vigenere
 	
 	def div_mensaje
 		@div_mensaje=@mensaje.scan(/.{1,#{@clave.length}}/)
+        temp=[]
+        for i in (0..@div_mensaje.length-1)
+            temp.push(@div_mensaje[i].to_s.split(''))
+		end
+        @div_mensaje=temp
 	end
 	
 end
