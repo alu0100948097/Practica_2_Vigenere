14d1447cf1db7c6e05177298d658787fdcc03e8a
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2018-02-11 12:36:06 +0000

    Mostrando mensaje original dividido

--- lib/vigenere.rb
+++ lib/vigenere.rb
@@ -20,12 +20,34 @@ class Vigenere
 	end
 	
 	def div_mensaje
+        
 		@div_mensaje=@mensaje.scan(/.{1,#{@clave.length}}/)
         temp=[]
+        
         for i in (0..@div_mensaje.length-1)
             temp.push(@div_mensaje[i].to_s.split(''))
 		end
+        
         @div_mensaje=temp
+        
+        for i in (0..@div_mensaje.length-1)
+            for j in (0..@div_mensaje[i].length-1)
+                print "#{@div_mensaje[i][j]}".blue
+            end
+            print " "
+		end
+        
+        puts
+        
+        for i in (0..@div_mensaje.length-1)
+            for j in (0..@div_mensaje[i].length-1)
+                print "#{@clave[j]}".red
+            end
+            print " "
+		end
+        
+        puts
+        
 	end
 	
 end
